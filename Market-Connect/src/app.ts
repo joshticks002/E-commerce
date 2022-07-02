d9c0db6d127d0871126201def9d5ca1eb739bfe8
Joshua <jadeyemo002@gmail.com>
2022-07-02 11:08:19 +0100

    switched from pug to ejs templating engine

--- Market-Connect/src/app.ts
+++ Market-Connect/src/app.ts
@@ -1,4 +1,3 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -7,7 +6,7 @@ const dotenv = require('dotenv').config()
 const { connectDB } = require('./mongoose/db')
 connectDB()
 import { Request, Response, NextFunction } from 'express'
-import { HttpError } from 'http-errors'
+const { errorHandler } = require('./middlewares/errorHandler');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -22,25 +21,23 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
+
 
 app.use('/api/products', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req: Request, res: Response, next: NextFunction) {
-  next(createError(404));
+app.use('/home', (req: Request, res: Response) => {
+  res.status(404).json({
+      message: 'Welcome to Market-Connect' 
+  })
 });
 
-// error handler
-app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(errorHandler)
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        message: 'OOPS, page not found :)' 
+    })
+    next()
+})
 
 export default app;
